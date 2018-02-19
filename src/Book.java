25885e7a97c0358c88e8314a9196bdd278f91299
Chris <christopheramott@sky.com>
2018-02-19 11:13:50 +0000

    Encapsulated Book file loader to Book class as a static function

--- src/Book.java
+++ src/Book.java
@@ -1,3 +1,9 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Book {
     private int id;
     private String name;
@@ -18,4 +24,39 @@ public class Book {
     public String getName() {
         return name;
     }
+
+    public void setAvailable(int available) {
+        this.available = available;
+    }
+
+    public static List<Book> read(String file){
+        BufferedReader br = null;
+        String line;
+        List<Book> books = new ArrayList<>();
+        try {
+            br = new BufferedReader(new FileReader(file));
+
+            while((line = br.readLine()) != null){
+                String[] book = line.split(",");
+                books.add(new Book(
+                        Integer.parseInt(book[0]),
+                        book[1],
+                        book[2],
+                        Integer.parseInt(book[3]),
+                        Integer.parseInt(book[4])));
+            }
+        }catch (IOException e){
+            e.printStackTrace();
+        }finally {
+            if (br != null){
+                try{
+                    br.close();
+                }catch (IOException e){
+                    e.printStackTrace();
+                }
+            }
+        }
+        return books;
+    }
+
 }
--- src/Library.java
+++ src/Library.java
@@ -1,10 +1,6 @@
 import java.io.*;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class Library {
 
@@ -13,31 +9,7 @@ public class Library {
     List<BookLoan> bookloans = new ArrayList<>();
 
     public Library(String booksFileName, String membersFileName, String bookLoansFileName){
-        BufferedReader br = null;
-        String line;
-        try {
-            br = new BufferedReader(new FileReader(booksFileName));
-
-            while((line = br.readLine()) != null){
-                String[] book = line.split(",");
-                books.add(new Book(
-                        Integer.parseInt(book[0]),
-                        book[1],
-                        book[2],
-                        Integer.parseInt(book[3]),
-                        Integer.parseInt(book[4])));
-            }
-        }catch (IOException e){
-            e.printStackTrace();
-        }finally {
-            if (br != null){
-                try{
-                    br.close();
-                }catch (IOException e){
-                    e.printStackTrace();
-                }
-            }
-        }
+        books = Book.read(booksFileName);
     }
     public List<Book> showAllBooks(){
         return books;
